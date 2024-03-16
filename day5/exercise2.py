df5aec2728624e8bbe2f44605c67096d5bb826ff
Akelvino <vinho892@gmail.com>
2024-03-16 17:45:27 +0300

    Finished day 5

---
+++ day5/exercise2.py
@@ -0,0 +1,11 @@
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+print(max(ages))
+print(min(ages))
+# num1 = min(ages)
+# num2 = max(ages)
+
+print(max(ages)+min(ages))
+
+print(ages[len(ages)//2])
+
+print(sum(ages)/2)
